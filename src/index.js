2daddcdcbf5a1b56dee4b9e02efb38e6886c5f7e
SmitTester <smittester321@gmail.com>
2026-07-16 17:26:38 +0530

    Added apiError and apiResponse kandler and modified app.js constructorconstructor -ST

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+import { app } from "./app.js";
 import { connectDB } from "./db/index.js";
 import dotenv from 'dotenv';
 dotenv.config({path: './env'});
 
 
-connectDB();
+connectDB().then(() => {
+    app.listen(process.env.PORT || 3000,() => {
+     console.log(`Servie is started on ${process.env.PORT}`)
+    })
+}).catch((error) => console.log('Mongodb connection failed--',error));
 
