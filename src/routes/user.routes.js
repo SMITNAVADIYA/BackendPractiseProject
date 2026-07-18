94bc5071926a6be4fb7570fe9e13313a9c0484bf
SmitTester <smittester321@gmail.com>
2026-07-18 14:47:34 +0530

    Added register controller with routes for testing

---
+++ src/routes/user.routes.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { registerUser } from '../controllers/user.controller.js';
+
+
+export const router = Router();
+
+router.route('/register').post(registerUser)
+
+
+
+
+
+
+
+
+
+
+
+
+
