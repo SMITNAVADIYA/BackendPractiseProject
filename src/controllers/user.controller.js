5b487bdab1a42ce689928974227bee8dce2095ba
SmitTester <smittester321@gmail.com>
2026-07-18 20:40:39 +0530

    Fixed response sending issue for register user and correct ApiRespone grammetical error & added comments in user-Controller -ST

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,18 +2,17 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 import { ApiError } from "../utils/apiError.js";
 import { User } from "../models/user/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
-import { ApiRespone } from "../utils/ApiResponse.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 
 const registerUser = asyncHandler(async (req, res) => {
+
+
+  // Get details from frontend
   const { fullName, userName, email, password } = req.body;
-  console.log('Fullname---', fullName);
-  console.log('User name---', userName);
-  console.log('Email---', email);
-  console.log('Password---', password);
 
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+  // Check any field is empty OR skip by client and throw error
   if (
     [fullName, userName, email, password].some(
       (field) => !field || field.trim() === ""
@@ -21,33 +20,43 @@ const registerUser = asyncHandler(async (req, res) => {
   ) {
     throw new ApiError(400, "All fields are required");
   }
+
+
+  // Regex for check email format & throw error msg for inValid email
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(email)) {
     throw new ApiError(400, 'Please enter valid email')
   }
 
-  const existedUser = User.find({
+
+
+  //  Check for user already exist and throw error if it is
+  const existedUser = await User.findOne({
     $or: [{ userName }, { email }]
   });
-
   if (existedUser) {
     throw new ApiError(409, 'User is already exist')
   }
 
+  // Get local path for avatar and cover image from client & throw error as avatar is required
   const avatarLocalPath = req.files?.avatar[0]?.path;
   const coverImageLocalPath = req.files?.coverImage[0]?.path;
-
-
   if (!avatarLocalPath) {
     throw new ApiError(400, 'Avatar is required')
   }
 
+
+
+  // Upload avatar & cover image to cloudinary and throw error for avatar required
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
-
   if (!avatar) {
-    throw new ApiError(400,'Avatar file is required')
+    throw new ApiError(400, 'Avatar file is required')
   }
 
+
+
+  // Doing entry to database
   const user = await User.create({
     fullName,
     avatar: avatar.url,
@@ -56,13 +65,15 @@ const registerUser = asyncHandler(async (req, res) => {
     email,
     password,
   })
-  const createdUser = await User.findById(user._id).select("-password -refreshToken");
 
-  if(!createdUser){
-    throw new ApiError(500,'Internal server error')
+
+  // Removed password and refresh token from response as it's not good practise & throw error
+  const createdUser = await User.findById(user._id).select("-password -refreshToken");
+  if (!createdUser) {
+    throw new ApiError(500, 'Internal server error')
   }
 
-  return ApiRespone(200, createdUser , 'User registered successfully!!!')
+  return res.status(201).json(new ApiResponse(200, createdUser, 'Usr registered successfully!!!'))
 })
 
 export { registerUser }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,24 @@
 import { Router } from 'express';
 import { registerUser } from '../controllers/user.controller.js';
+import { upload } from '../middlewars/multer.middleware.js';
 
 
 export const router = Router();
 
-router.route('/register').post(registerUser)
+router.route('/register').post(
+    upload.fields(
+        [
+            {
+              name: 'avatar',
+              maxCount: 1
+            },
+            {
+              name: 'coverImage',
+              maxCount: 1
+            }
+        ]
+    ),
+    registerUser)
 
 
 
